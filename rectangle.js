d71021b810b8429082fdf635a8a8ea6dd97bdbf3
ttakeyaya <taketakemoji@gmail.com>
2021-12-25 14:25:05 +0900

    Update sample files

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,16 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) => {
+  if(x <= 0 | y <= 0){
+    setTimeout(() => 
+      callback(new Error("The given values must be greater than zero"), null),
+      2000
+    );
+  }else{
+    setTimeout(() => 
+      callback(null,{
+        perimeter: (x,y) => (2*(x+y)),
+        area : (x,y) => (x*y),
+      }),
+      2000
+    );
+  }
+}
